78b8cfea7b78dbc8136048e1cf9ea38f3ed797d0
saicharan pabbathi <saicharan.pabbathi@zemosolabs.com>
2023-03-29 12:37:01 +0530

    added the arrays.fill methord and gave the time and space complexity and added and commented the alternate method

--- java assignments/assignments/assignment2/SearchAlphabets.java
+++ java assignments/assignments/assignment2/SearchAlphabets.java
@@ -1,28 +1,26 @@
 package com.assignments.assignment2;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class SearchAlphabets {
     public static void main(String args[])
     {
-        Scanner sc=new Scanner(System.in);
+        Scanner inputScanner=new Scanner(System.in);
         System.out.println("enter the string to search");
-        String string=new String(sc.nextLine().toLowerCase());
-        byte b[]=string.getBytes();
+        String inputString=new String(inputScanner.nextLine().toLowerCase());
+        byte stringBytes[]=inputString.getBytes();
         int count=26;
         boolean findAlphas[]=new boolean[26];
-        for(int i=0;i<26;i++)
-        {
-            findAlphas[i]=false;
-        }
+        Arrays.fill(findAlphas,false);
         int i=0;
-        while(count!=0&&i<string.length())
+        while(count!=0&&i<inputString.length())
         {
-            if(b[i]>=97&&b[i]<=122)
+            if(stringBytes[i]>=97&&stringBytes[i]<=122)
             {
-                if(!findAlphas[b[i]-97])
+                if(!findAlphas[stringBytes[i]-97])
                 {
-                    findAlphas[b[i]-97]=true;
+                    findAlphas[stringBytes[i]-97]=true;
                     count--;
                 }
             }
@@ -30,11 +28,46 @@ public class SearchAlphabets {
         }
         if(count==0)
         {
-            System.out.println(string+" contains all alphabets");
+            System.out.println(inputString+" contains all alphabets");
         }
         else {
-            System.out.println(string+" does not contain all alpha bets");
+            System.out.println(inputString+" does not contain all alpha bets");
         }
+        // alternateMethod();
+	//time complexity of this method is O(n) where n is the length of the string
+	//best case scenario O(26)
+	//worst case scenario O(n)
+	//average case scenario O(n)
+    //space complexity is O(n) for the byte array
 
     }
+
+    // public static void alternateMethod()
+    // {
+    //     Scanner sc=new Scanner(System.in);
+    //     System.out.println("enter the input string:");
+    //     String input=new String(sc.nextLine().toLowerCase());
+    //     String alphabets="abcdefghijklmnopqrstuvwxyz";
+    //     int count=0;
+    //     for(int i=0;i<26;i++)
+    //     {
+    //         if(input.indexOf(alphabets.charAt(i))<0)
+    //         {
+    //             break;
+    //         }
+    //         else{
+    //             count++;
+    //         }
+    //     }
+
+    //     if(count==26)
+    //     {
+    //         System.out.println(input+"  contains all alphabets");
+    //     }
+    //     else{
+    //         System.out.println(input+" string does not contain all alphabets");
+    //     }
+    //     //time complexity of the above code is O(26n)
+    //     //space complexity is O(1)
+    // }
 }
